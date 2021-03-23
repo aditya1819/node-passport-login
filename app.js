dedbd1577ab07c54953156899c8b3cfe98ab304c
aditya1819 <shahare.aditya@gmail.com>
2021-03-23 22:56:02 +0530

    routes added

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 app = express();
 
+// Routes 
+
+const indexRoutes = require('./routes/index')
+const userRoutes = require('./routes/user')
+
+
+app.use('/', indexRoutes)
+
+app.use('/user', userRoutes)
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, console.log(`Server Started on PORT : ${PORT}`));
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+    res.send("Login Page")
+})
+
+router.get('/register', (req, res) => {
+    res.send("Registraion page")
+})
+
+
+module.exports = router;
