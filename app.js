3aae20a031b1b54c63cb136b65cbbb77c60b3584
aditya1819 <shahare.aditya@gmail.com>
2021-03-24 22:49:56 +0530

    registration complete

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const expressLayout = require('express-ejs-layouts')
-const mongoose = require('mongoose')
+const expressLayout = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 
 app = express();
 
@@ -19,6 +22,22 @@ app.set('view engine', 'ejs')
 // BodyParser 
 app.use(express.urlencoded({ extended : false }));
 
+// Express session 
+app.use(session({
+    secret: 'secretKey123',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+// connect flash
+app.use(flash());
+
+// Global vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+})
 
 // Routes 
 const indexRoutes = require('./routes/index')
